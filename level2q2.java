f29a414e11436cdc9d8278dabc3e9a65384cdb1b
gr7347-byte <gajjalakoushiktejreddy@gmail.com>
2026-03-02 22:07:06 +0530

    l1q2

---
+++ level2q2.java
@@ -0,0 +1,12 @@
+class AverageMarks {
+    public static void main(String[] args) {
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+        int totalSubjects = 3;
+
+        double averageMark = (maths + physics + chemistry) / (double) totalSubjects;
+
+        System.out.println("Sam's average mark in PCM is " + averageMark);
+    }
+}
